b9ba172ae6fa47ee8a97aab57504807b0d3fda2f
rashmiw333 <92781108+rashmiw333@users.noreply.github.com>
2026-06-26 14:33:54 +0530

    Updating backend books with CORS

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const {initializeDatabase} = require("./db/db.connect");
 const Book = require("./models/book.models");
 
-app.use(express.json());
+ app.use(express.json());
+ app.use(cors());
 
 initializeDatabase();
 
